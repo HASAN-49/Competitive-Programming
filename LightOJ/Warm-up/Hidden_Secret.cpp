086f5ca3c0cd9d9cdd07f370f534a446e3ee762c
Mahmudul Hasan Bhuiyan <107288719+HASAN-49@users.noreply.github.com>
2023-11-23 18:50:59 +0600

    Solution of LightOJ Problem Hidden Secret

---
+++ LightOJ/Warm-up/Hidden_Secret.cpp
@@ -0,0 +1,64 @@
+// بسم الله الرحمن الرحيم
+// Solution of LightOJ Problem Hidden Secret
+#include<bits/stdc++.h>
+using namespace std ;
+
+#define test int t; for(cin >> t; t; t--)
+#define ll long long
+#define ld long double
+
+#define all(a) a.begin(), a.end()
+#define allr(a) a.rbegin(), a.rend()
+#define out(x) cout << x << endl
+#define outs(x) cout << x << " "
+
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define nline cout << "\n"
+
+#define pi 3.14159265358979323846
+#define MOD 1000000007
+
+void solve(int tt) 
+{
+    string a, b;
+    getline(cin >> ws, a);
+    getline(cin >> ws, b);
+
+    string aa = "", bb = "";
+    int sz = a.length();
+    
+    for(int i = 0; i < sz; i++) {
+        if(a[i] == ' ') continue;
+        aa += tolower(a[i]);
+    }
+    sort(all(aa));
+    
+    sz = b.length();
+    for(int i = 0; i < sz; i++) {
+        if(b[i] == ' ') continue;
+        bb += tolower(b[i]);
+    }
+    sort(all(bb));
+    
+    cout << "Case " << tt << ": ";
+    if(aa == bb) cout << "Yes\n";
+    else cout << "No\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    // sieve();
+    int k = 1;
+    test solve(k++);
+    // cout << "HASAN\n";
+    return 0;
+}
