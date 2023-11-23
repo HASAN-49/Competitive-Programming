2d9bc61b2b16ee7e92ed450594dab30bcd479dd0
Mahmudul Hasan Bhuiyan <107288719+HASAN-49@users.noreply.github.com>
2023-11-23 08:00:00 +0600

    Solution of CodeChef problem Worth of a Video

---
+++ CodeChef/Worth_of_a_Video.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        long long s;
+        cin >> s;
+        cout << s * 24000 << endl;
+    }
+	return 0;
+}
