2d8b35846a9eb2fcfd330a4b039d9afbeb35e9f3
Mahmudul Hasan Bhuiyan <107288719+HASAN-49@users.noreply.github.com>
2025-04-29 06:08:30 +0600

    Solution of LightOJ Problem Opposite Task

---
+++ LightOJ/Warm-up/Opposite_Task.cpp
@@ -0,0 +1,23 @@
+// Problem Link --> https://lightoj.com/problem/opposite-task
+
+// بسم الله الرحمن الرحيم
+#include<bits/stdc++.h>
+using namespace std;
+
+#define test int t; for(cin >> t; t; t--)
+
+void solve(int tc) {
+  int n; cin >> n;
+  cout << min(n, 10) << ' ' << n - min(n, 10) << '\n';
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int k = 1;
+  test 
+  solve(k++);
+
+  return 0;
+}
